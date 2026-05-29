aac914e87ccca3d6bcd1c5dc8ca3d36f2991d48f
Berlin <berlinyesubatham@gmail.com>
2026-05-29 12:29:35 +0530

    fixed mysql deployment

--- backend/app.py
+++ backend/app.py
@@ -31,6 +31,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+with app.app_context():
+    db.create_all()
+
 class Booking(db.Model):
     __tablename__ = "booking"
     id       = db.Column(db.Integer, primary_key=True)
